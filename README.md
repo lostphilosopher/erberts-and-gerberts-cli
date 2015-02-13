e291182bf6810ca352d9a6c0a95748f4073f51b3
CBwyatt <wandersen@caringbridge.org>
2015-02-12 22:49:19 -0600

    Manage sandwiches soups and extras with arrays

--- README.md
+++ README.md
@@ -11,10 +11,13 @@ Step 2. Create a config.json with the following fields filed in:
     "address2" : "Apt. 42",
     "city" : "Anywhere",
     "zip" : "12345",
-    "contact" : "Amce Co"
+    "contact" : "Amce Co",
+    "sandwiches" : ["titan", "bluefinger"],
+    "soups" : ["broccoli"],
+    "extras" : ["chips"]    
 }
 ```
-Step 3. `ruby ryatt.rb`
+Step 3. `ruby ryatt.rb` (make sure you have ruby and watir-webdriver installed)
 
 Step 4. Hit continue 
 
--- ryatt.rb
+++ ryatt.rb
@@ -27,26 +27,59 @@ config = JSON.parse(contents)
 @b.img(:title => 'Check Address').click
 @b.execute_script "window.alert = function() { return true; }"
 
+# Yes twice, for reasons
 @b.button(:title => 'Continue').click
 @b.button(:title => 'Continue').click
 
-# Wyatt
-@b.goto "https://erbertandgerberts-delivery-1088.patronpath.com/order.php?rid=a6f9abf5979ea03ec084f4b266e1bb6a&cmd=view&cat=c50c207dc0d9da97329a89e64a589cad&iid=64eaa19b89e0fa959d1299758e624782"
-@b.select_list(:id => 'item_options[]').select_value('099a429f2f463b6a0078d6cc9c7844bd')
-@b.checkbox(:id => 'box4-0').set
-@b.text_field(:name => 'label').set 'Wyatt'
-@b.button(:id => 'AddToPlate').click
+# Order sandwiches
+config['sandwiches'].each do |item|
+    puts "Ordering: #{item}"
 
-# Ryan
-@b.goto "https://erbertandgerberts-delivery-1088.patronpath.com/order.php?rid=a6f9abf5979ea03ec084f4b266e1bb6a&cmd=view&cat=c50c207dc0d9da97329a89e64a589cad&iid=976db75c0f415efba1cbae6c1f89a049"
-@b.select_list(:id => 'item_options[]').select_value('099a429f2f463b6a0078d6cc9c7844bd')
-@b.checkbox(:id => 'box1-0').set
-@b.text_field(:name => 'label').set 'Ryan'
-@b.button(:id => 'AddToPlate').click
-@b.goto "https://erbertandgerberts-delivery-1088.patronpath.com/order.php?rid=a6f9abf5979ea03ec084f4b266e1bb6a&cmd=view&cat=4d8e4fae02cf0066f7a14f2951560fd2&iid=38479c92bf07cf956a955615c82b5560"
-@b.select_list(:id => 'item_options[]').select_value('28e90113ff0165a922ad0a70d0b247f6')
-@b.text_field(:name => 'label').set 'Ryan'
-@b.button(:id => 'AddToPlate').click
+    case item
+        when "titan"
+            @b.goto "https://erbertandgerberts-delivery-1088.patronpath.com/order.php?rid=a6f9abf5979ea03ec084f4b266e1bb6a&cmd=view&cat=c50c207dc0d9da97329a89e64a589cad&iid=64eaa19b89e0fa959d1299758e624782"
+            @b.select_list(:id => 'item_options[]').select_value('099a429f2f463b6a0078d6cc9c7844bd')
+            @b.button(:id => 'AddToPlate').click
+
+        when "bluefinger"
+            @b.goto "https://erbertandgerberts-delivery-1088.patronpath.com/order.php?rid=a6f9abf5979ea03ec084f4b266e1bb6a&cmd=view&cat=c50c207dc0d9da97329a89e64a589cad&iid=976db75c0f415efba1cbae6c1f89a049"
+            @b.select_list(:id => 'item_options[]').select_value('099a429f2f463b6a0078d6cc9c7844bd')
+            @b.checkbox(:id => 'box1-0').set
+            @b.button(:id => 'AddToPlate').click
+            
+        else
+          puts "NAS: #{item} is not a sandwhich"
+    end    
+end
+
+# Order soups
+config['soups'].each do |item|
+    puts "Ordering: #{item}"
+
+    case item
+        when "broccoli"
+            @b.goto "https://erbertandgerberts-delivery-1088.patronpath.com/order.php?rid=a6f9abf5979ea03ec084f4b266e1bb6a&cmd=view&cat=4d8e4fae02cf0066f7a14f2951560fd2&iid=38479c92bf07cf956a955615c82b5560"
+            @b.select_list(:id => 'item_options[]').select_value('28e90113ff0165a922ad0a70d0b247f6')
+            @b.button(:id => 'AddToPlate').click
+            
+        else
+          puts "NAS: #{item} is not a soup"
+    end    
+end
+
+# Extras
+config['extras'].each do |item|
+    puts "Ordering: #{item}"
+
+    case item
+        when "chips"
+            @b.goto "https://erbertandgerberts-delivery-1088.patronpath.com/order.php?rid=a6f9abf5979ea03ec084f4b266e1bb6a&cmd=view&cat=3eb80a494213cfc2bc29e95b2a57d011&iid=1f9c3e07f2f6928b5cd5c24e5e4e1567"
+            @b.button(:id => 'AddToPlate').click
+            
+        else
+          puts "NAS: #{item} is not a side"
+    end    
+end
 
 @b.goto "https://erbertandgerberts-delivery-1088.patronpath.com/checkout.php"
 @b.text_field(:name => 'contactname').set config['contact']
